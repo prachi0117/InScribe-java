d9afe4134dbb2ccbc7aa6bbe44068b9a1c42f3a9
prachi gupta <prachiannie17@gmail.com>
2024-09-04 15:11:40 +0530

    second commit

--- src/main/java/com/programming/springblog/controller/ApiResponse.java
+++ src/main/java/com/programming/springblog/controller/ApiResponse.java
@@ -5,13 +5,25 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class ApiResponse {
 
 	private String message;
 	private boolean success;
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public boolean isSuccess() {
+		return success;
+	}
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+	public ApiResponse(String message, boolean success) {
+		this.message = message;
+		this.success = success;
+	}
 }
 
